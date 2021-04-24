1145e1e516c45080b3ed898c1c10491c7ce37835
HxM <anthony.cuttivet@gmail.com>
2021-04-24 19:55:42 +0200

    Pop up as prefab + close on click

--- Assets/Scripts/UI/Elems/PopUpController.cs
+++ Assets/Scripts/UI/Elems/PopUpController.cs
@@ -7,8 +7,24 @@ public class PopUpController : MonoBehaviour
 
     public float MovementValue;
     public float MovementDuration;
+    
+    private Vector3 basePos;
 
-    public void Awake(){
+
+
+    public void OnEnable(){
+
+        basePos = this.transform.position;  
+
         MovementUtils.MoveY(this.gameObject, this.MovementValue, this.MovementDuration);
+    }
+
+    public void ClickOnClose(){
+        MovementUtils.MoveY(this.gameObject, -this.MovementValue, this.MovementDuration / 1.5f, Deactivate);
+    }
+
+    public void Deactivate(){
+        this.gameObject.SetActive(false);
+        this.transform.position = basePos;
     }
 }
--- Assets/Scripts/Utils/MovementUtils.cs
+++ Assets/Scripts/Utils/MovementUtils.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using System;
 
 public static class MovementUtils
 {
-    public static void MoveY(GameObject target, float value, float duration){
+    public static void MoveY(GameObject target, float value, float duration, Action callback = null){
 
-        target.transform.DOMoveY(target.transform.position.y + value, duration).SetEase(Ease.OutCubic);
+        target.transform.DOMoveY(target.transform.position.y + value, duration).SetEase(Ease.OutCubic).OnComplete(()=>{
+            callback?.Invoke();
+        });
     }
 }
