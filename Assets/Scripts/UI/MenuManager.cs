ea044edd27ff262413606654aefc23a21361f135
Yoann <yomaisonneuve@gmail.com>
2021-04-25 17:07:41 +0200

    Improve SoundMenu, import Sound menu layout

--- Assets/Scripts/UI/MenuManager.cs
+++ Assets/Scripts/UI/MenuManager.cs
@@ -1,63 +1,68 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class MenuManager : MonoBehaviour
 {
-    private static MenuManager _instance;
-
-    [SerializeField] private GameObject startMenu;
-    [SerializeField] private GameObject inGameMenu;
     [SerializeField] private GameObject soundMenu;
+    [SerializeField] private List<GameObject> mainVolumeBars;
+    [SerializeField] private List<GameObject> musicVolumeBars;
+    [SerializeField] private List<GameObject> sfxVolumeBars;
 
-    private GameObject lastMenu;
-    private void Awake()
-    {
-        if (_instance)
-        {
-            Destroy(gameObject);
-        }
-        else
-        {
-            DontDestroyOnLoad(gameObject);
-            _instance = this;
-        }
-    }
-
-    private void Start()
-    {
-        lastMenu = startMenu;
-    }
-
+    private int nbMainVolumeBars = 20;
+    private int nbMusicVolumeBars = 20;
+    private int nbSFXVolumeBars = 20;
+    
     public void OnPauseInGame()
     {
-        lastMenu = inGameMenu;
-        inGameMenu.SetActive(!inGameMenu.activeSelf);
+        soundMenu.SetActive(!soundMenu.activeSelf);
     }
 
-    public void OnStartGame()
+    public void OnMainVolumeClick(int value)
     {
-        startMenu.SetActive(false);
+        print(value);
+        nbMainVolumeBars = Mathf.Clamp(nbMainVolumeBars + value, 0, 20);
+        SoundManager.UpdateMainVolume(nbMainVolumeBars/20f);
+        int i;
+        for (i = 0; i < nbMainVolumeBars; i++)
+        {
+            mainVolumeBars[i].SetActive(true);
+        }
+        for (i = nbMainVolumeBars; i < mainVolumeBars.Count; i++)
+        {
+            mainVolumeBars[i].SetActive(false);
+        }
     }
-
-    public void OnQuitGame()
+    
+    public void OnMusicVolumeClick(int value)
     {
-        Application.Quit();
+        nbMusicVolumeBars = Mathf.Clamp(nbMusicVolumeBars + value, 0, 20);
+        SoundManager.UpdateMainVolume(nbMusicVolumeBars/20f);
+        int i;
+        for (i = 0; i < nbMusicVolumeBars; i++)
+        {
+            musicVolumeBars[i].SetActive(true);
+        }
+        for (i = nbMusicVolumeBars; i < musicVolumeBars.Count; i++)
+        {
+            musicVolumeBars[i].SetActive(false);
+        }
     }
-
-    public void OnResumeGame()
+    
+    public void OnSFXVolumeClick(int value)
     {
-        inGameMenu.SetActive(false);
+        nbSFXVolumeBars = Mathf.Clamp(nbSFXVolumeBars + value, 0, 20);
+        SoundManager.UpdateMainVolume(nbSFXVolumeBars/20f);
+        int i;
+        for (i = 0; i < nbSFXVolumeBars; i++)
+        {
+            sfxVolumeBars[i].SetActive(true);
+        }
+        for (i = nbSFXVolumeBars; i < sfxVolumeBars.Count; i++)
+        {
+            sfxVolumeBars[i].SetActive(false);
+        }
     }
-
-    public void OnBackMenu()
-    {
-        soundMenu.SetActive(false);
-        lastMenu.SetActive(true);
-    }
-
-    public void OnSoundMenu()
-    {
-        lastMenu.SetActive(false);
-        soundMenu.SetActive(true);
-    }
+    
+    
 }
